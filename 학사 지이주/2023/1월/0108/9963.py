c8aab75278c299f0eece3990ecbbb847435496a8
JiIJu <109330668+JiIJu@users.noreply.github.com>
2023-01-08 15:45:34 +0900

    0108

---
+++ 학사 지이주/2023/1월/0108/9963.py
@@ -0,0 +1,32 @@
+# 9663 N-Queen
+
+def visit(x,y,visited):
+    check = [[0]*N for _ in range(N)]
+    for i in range(N):
+        for j in range(N):
+            check[i][j]=visited[i][j]
+    for i in range(1,N-x):
+        check[x+i][y] = 1
+        if 0<= y-i < N:
+            check[x+i][y-i] = 1
+        if 0<= y+i <N:
+            check[x+i][y+i] = 1
+    return check
+
+def dfs(x,visited):
+    global cnt
+    for i in range(x,N):
+        if sum(visited[x])==N:
+            return
+    if x==N-1:
+        cnt += N - sum(visited[x])
+        return 0
+    for i in range(N):
+        if visited[x][i]==0:
+            tmp = visit(x,i,visited)
+            dfs(x+1,tmp)
+N = int(input())
+data = [[0]*N for _ in range(N)]
+cnt = 0
+dfs(0,data)
+print(cnt)
