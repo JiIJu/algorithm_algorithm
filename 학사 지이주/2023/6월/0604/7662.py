476a58c1d7c29254b0bd5d97de5967ef876554a3
wldlwn12@naver.com <wldlwn12@naver.com>
2023-06-04 17:05:33 +0900

    0604

---
+++ 학사 지이주/2023/6월/0604/7662.py
@@ -0,0 +1,36 @@
+# 7662 이중 우선순위 큐
+import sys
+import heapq
+T = int(input())
+
+for tc in range(T):
+    k = int(input())
+    visit = [0]*k
+    maxV,minV = [],[]
+    for i in range(k):
+        I,n = sys.stdin.readline().split()
+        n = int(n)
+        if I=='I':
+            heapq.heappush(maxV,(-n,i))
+            heapq.heappush(minV, (n, i))
+            visit[i]=1
+        elif I=='D' and n==1:
+            while maxV and not maxV[0][1]:
+                heapq.heappop(maxV)
+            if maxV:
+                visit[maxV[0][1]]=0
+                heapq.heappop(maxV)
+        elif I=='D' and n==-1:
+            while minV and not minV[0][1]:
+                heapq.heappop(minV)
+            if minV:
+                visit[minV[0][1]]=0
+                heapq.heappop(minV)
+    if maxV and visit[maxV[0][1]]==0:
+        heapq.heappop(maxV)
+    if minV and visit[minV[0][1]]==0:
+        heapq.heappop(minV)
+    if minV and maxV:
+        print(-maxV[0][0],minV[0][0])
+    else:
+        print('EMPTY')
