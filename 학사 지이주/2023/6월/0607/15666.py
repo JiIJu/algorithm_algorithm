79fcf87f6c4403a4f4e3e85c2919c7835c2622d4
wldlwn12@naver.com <wldlwn12@naver.com>
2023-06-07 21:38:08 +0900

    0607

---
+++ 학사 지이주/2023/6월/0607/15666.py
@@ -0,0 +1,16 @@
+# 15666 N과M (12)
+
+
+N,M = map(int,input().split())
+data = sorted(set(map(int,input().split())))
+ans = []
+def solve(n):
+    if n==M:
+        print(*ans)
+        return
+    for i in range(len(data)):
+        if n==0 or ans[-1]<=data[i]:
+            ans.append(data[i])
+            solve(n+1)
+            ans.pop()
+solve(0)
