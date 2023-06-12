993871ba964af4fe908108e715d421071ec55bdc
JiIJu <109330668+JiIJu@users.noreply.github.com>
2023-06-12 16:40:40 +0900

    0612

---
+++ 학사 지이주/2023/6월/0612/5001.py
@@ -0,0 +1,26 @@
+# 5001 트리의 부모찾기
+import sys
+from collections import deque
+def bfs(n):
+    visit = [0] * (N + 1)
+    q = deque([n])
+    visit[n]=1
+    while q:
+        a = q.popleft()
+        for i in data[a]:
+            if not visit[i]:
+                visit[i]=1
+                q.append(i)
+                ans[i]=a
+N = int(input())
+data = [[] for _ in range(N+1)]
+for i in range(N-1):
+    a,b = map(int,sys.stdin.readline().split())
+    data[a].append(b)
+    data[b].append(a)
+ans = [0]*(N+1)
+# for i in range(1,N+1):
+bfs(1)
+print(ans)
+for i in range(2,N+1):
+    print(ans[i])
