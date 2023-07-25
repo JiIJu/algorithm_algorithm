0253dd315095d61dddbabd7fe16c76b0ddb2ea0c
JiIJu <109330668+JiIJu@users.noreply.github.com>
2023-07-25 16:45:43 +0900

    0725

---
+++ 학사 지이주/2023/7월/0725/1991.py
@@ -0,0 +1,30 @@
+# swea 1991 트리순회
+def pre(a):
+    if a!='.':
+        print(a,end='')
+        pre(data[a][0])
+        pre(data[a][1])
+
+def pre2(a):
+    if a!='.':
+        pre2(data[a][0])
+        print(a, end='')
+        pre2(data[a][1])
+
+def pre3(a):
+    if a!='.':
+        pre3(data[a][0])
+        pre3(data[a][1])
+        print(a, end='')
+N = int(input())
+data = {}
+
+for i in range(N):
+    root,left,right = input().split()
+    data[root] = [left,right]
+
+pre('A')
+print()
+pre2('A')
+print()
+pre3('A')
