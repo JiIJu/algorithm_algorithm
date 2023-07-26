7f8aac6a5b1af6744daa3e3eda3ed09c0e145e7a
wldlwn12@naver.com <wldlwn12@naver.com>
2023-07-26 13:35:00 +0900

    0726

---
+++ 학사 지이주/2023/7월/0726/1247.py
@@ -0,0 +1,25 @@
+# swea 1247 최적경
+def dfs(now,check,visit):
+    global minv
+    if check>=minv:
+        return
+    if sum(visit)==N:
+        maxv = min(minv,check + abs(now[0]-home[0])+abs(now[1]-home[1]))
+        return
+    for i in range(N):
+        if not visit[i]:
+            visit[i] = 1
+            dfs(data[i],check+abs(now[0]-data[i][0])+abs(now[1]-data[i][1]),visit)
+            visit[i] = 0
+T = int(input())
+for tc in range(1,T+1):
+    N = int(input())
+    temp = list(map(int,input().split()))
+    company = [temp[0],temp[1]]
+    home = [temp [2],temp[3]]
+    data = []
+    for i in range(4,N*2+4,2):
+        data.append([temp[i],temp[i+1]])
+    minv = 9999999
+    dfs((company[0],company[1]),0,[0]*N)
+    print(f'#{tc} {minv}')
