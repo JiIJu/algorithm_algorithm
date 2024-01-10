6f335943254ddb0521db7c4edc2cd1c2383c1ba1
Jiyoung Choi <77952928+JIYOUNG3347@users.noreply.github.com>
2024-01-10 22:03:36 +0900

    Update 20240110.py

--- 박사 최지영/202401/20240110.py
+++ 박사 최지영/202401/20240110.py
@@ -1,3 +1,10 @@
 def solution(s):
     arr = list(map(int, s.split(' ')))
     return str(min(arr)) + " " + str(max(arr))
+
+def solution(s):
+    s=s.split(' ')
+    for i in range(len(s)):
+        s[i]=s[i].capitalize()
+    answer=' '.join(s)
+    return answer
